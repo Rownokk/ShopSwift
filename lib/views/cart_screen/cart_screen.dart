3befcd7c97e33c3e63c8b88cd6520f4e2da9cab9
Mowmitaaa <rownok.cse.20220104151@aust.edu>
2024-01-20 10:25:02 +0600

    cart_screen

--- lib/views/cart_screen/cart_screen.dart
+++ lib/views/cart_screen/cart_screen.dart
@@ -5,8 +5,48 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.cyan,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your\'s Cart',style: TextStyle( color: Colors.white),),
+      ),
+      backgroundColor: Colors.pinkAccent, // Set background color for the entire screen
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.shopping_cart,
+              size: 100,
+              color: Colors.white,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Your Cart is Empty',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement navigation or any action when the button is pressed
+              },
+              child: Text('Start Shopping'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: CartScreen(),
+    ),
+  );
+}
+
