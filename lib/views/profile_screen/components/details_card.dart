dac3b78a2b59247267c9f108fd95c3ba0b972b72
Mowmitaaa <rownok.cse.20220104151@aust.edu>
2024-01-20 10:12:56 +0600

    details_card

--- lib/views/profile_screen/components/details_card.dart
+++ lib/views/profile_screen/components/details_card.dart
@@ -4,9 +4,9 @@ Widget detailsCard({width, String? count, String? title}) {
   return Column(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      count!.text.fontFamily(bold).color(redColor).size(16).make(),
+      count!.text.fontFamily(bold).color(Colors.black).size(16).make(),
       5.heightBox,
-      title!.text.color(redColor).make(),
+      title!.text.color(Colors.black).make(),
     ],
   ).box.white.rounded.width(width).height(80).padding(const EdgeInsets.all(4)).make();
 }
